e10938a2dd8495e256e30099b66cd04e47465a7b
qiaoyue <qiaoyue@baidu.com>
2018-04-19 18:48:15 +0800

    update

--- src/ar.tensorflow.ts
+++ src/ar.tensorflow.ts
@@ -6,6 +6,7 @@ import Dataset from './dataset';
 
 /**
  * webar by tensorflow
+ * onehot softmax dense ???
  */
 
 const CONTROLS = ['up', 'left', 'right', 'down'];
@@ -14,6 +15,7 @@ export default class ART {
     webcam: any;
     dataset: any;
     mobilenet: any;
+    model: any;
     training = false;
     active = false;
     /**
@@ -137,6 +139,71 @@ export default class ART {
         $('#up,#left,#right,#down').on('mouseup', () => {
             $(document.body).attr('data-active', '');
             this.training = false;
+        });
+    }
+
+    /**
+     * s训练我们自己的模型数据
+     */
+    train() {
+        if (this.dataset.xs == null) {
+            throw new Error('Add some examples before training!');
+        }
+        
+        const dataset = this.dataset;
+        // Creates a 2-layer fully connected model. By creating a separate model,
+        // rather than adding layers to the mobilenet model, we "freeze" the weights
+        // of the mobilenet model, and only train weights from the new model.
+        this.model = tf.sequential({
+            layers: [
+                // Flattens the input to a vector so we can use it in a dense layer. While
+                // technically a layer, this only performs a reshape (and has no training
+                // parameters).
+                tf.layers.flatten({inputShape: [7, 7, 256]}),
+                // Layer 1
+                tf.layers.dense({
+                    units: 100,
+                    activation: 'relu',
+                    kernelInitializer: 'varianceScaling',
+                    useBias: true
+                }),
+                // Layer 2. The number of units of the last layer should correspond
+                // to the number of classes we want to predict.
+                tf.layers.dense({
+                    units: 4,
+                    kernelInitializer: 'varianceScaling',
+                    useBias: false,
+                    activation: 'softmax'
+                })
+            ]
+        });
+    
+        // Creates the optimizers which drives training of the model.
+        const optimizer = tf.train.adam(0.01);
+        // We use categoricalCrossentropy which is the loss function we use for
+        // categorical classification which measures the error between our predicted
+        // probability distribution over classes (probability that an input is of each
+        // class), versus the label (100% probability in the true class)>
+        this.model.compile({optimizer: optimizer, loss: 'categoricalCrossentropy'});
+    
+        // We parameterize batch size as a fraction of the entire dataset because the
+        // number of examples that are collected depends on how many examples the user
+        // collects. This allows us to have a flexible batch size.
+        const batchSize = Math.floor(dataset.xs.shape[0] * 0.04);
+        if (!(batchSize > 0)) {
+            throw new Error('Batch size is 0 or NaN. Please choose a non-zero fraction.');
+        }
+    
+        // Train the model! Model.fit() will shuffle xs & ys so we don't have to.
+        this.model.fit(dataset.xs, dataset.ys, {
+            batchSize,
+            epochs: 20,
+            callbacks: {
+                onBatchEnd: async (batch, logs) => {
+                    console.log('Loss: ' + logs.loss.toFixed(5));
+                    await tf.nextFrame();
+                }
+            }
         });
     }
 }
